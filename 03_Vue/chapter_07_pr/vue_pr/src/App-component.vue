c5d4fbe06ff9ec16538f3c6b0225a5f46671fc27
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-05-28 19:28:21 +0900

    App_Vue

---
+++ 03_Vue/chapter_07_pr/vue_pr/src/App-component.vue
@@ -0,0 +1,28 @@
+<script setup>
+import CheckList from './components/CheckList.vue';
+import TodoProps from './components/TodoProps.vue';
+</script>
+
+<style>
+.todo {
+    text-align: center;
+}
+</style>
+
+<template>
+    <div class="todo">
+        <h1>오늘의 할일</h1>
+        <CheckList />
+
+        <!-- <div class="checklist">
+            <h2>오늘 해야할 일 4개</h2>
+            <h2>오늘 완료한 일 0개</h2>
+        </div> -->
+        <hr />
+
+        <TodoProps todo="Vue 부수기" />
+
+        <TodoProps todo="배열함수 끝내기" />
+        <CheckList />
+    </div>
+</template>
---
+++ 03_Vue/chapter_07_pr/vue_pr/src/components/ColorEmit.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!--1번-->
+        <input type="text" v-model.trim="color" />
+        <button v-on:click="$emit('backGroundChange', { color })">
+            색상 전달
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ColorEmit',
+    data() {
+        return {
+            color: '',
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
