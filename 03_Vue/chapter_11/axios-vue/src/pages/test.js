b5c025921c39743fd529e073ed39532c47c1475d
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-06-04 17:25:07 +0900

    TodoList CRUD

---
+++ 03_Vue/chapter_11/axios-vue/src/pages/test.js
@@ -0,0 +1,13 @@
+const todo = {
+    id:'1',
+    todo: '오브젝트 이해하기',
+    done: false,
+}
+
+const copyTodo = {
+    ...todo.todo,
+    done: true,
+}
+
+console.log(todo === copyTodo)
+console.log(copyTodo)
